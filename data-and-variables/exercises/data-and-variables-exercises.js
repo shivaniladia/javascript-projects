ef4bb4a9ebf9b05a4a02752a5753e215410c45e8
shivaniladia <shivaniladia@gmail.com>
2024-01-11 17:21:06 -0600

    Assignment3 submitted

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,72 @@
-// Declare and assign the variables below
+let engineIndicatorLight = "red blinking";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
 
-// Use console.log to print the 'typeof' each variable. Print one item per line.
+if (engineIndicatorLight === "green") {
+    console.log("engines have started");
+ } else if (engineIndicatorLight === "green blinking") {
+    console.log("engines are preparing to start");
+ } else {
+    console.log("engines are off");
+ }
 
-// Calculate a space mission below
+ if (crewStatus) {
+    console.log("Crew Ready");
+  } else {
+    console.log("Crew Not Ready");
+  }
 
-// Print the results of the space mission calculations below
+  if (computerStatusCode === 200) {
+    console.log("Please stand by. Computer is rebooting.");
+  } else if (computerStatusCode === 400) {
+    console.log("Success! Computer online.");
+  } else {
+    console.log("ALERT: Computer offline!");
+  }
 
-// Calculate a trip to the moon below
+  if (shuttleSpeed > 17500) {
+    console.log("ALERT: Escape velocity reached!");
+  } else if (shuttleSpeed < 8000) {
+    console.log("ALERT: Cannot maintain orbit!");
+  } else {
+    console.log("Stable speed.");
+  }
 
-// Print the results of the trip to the moon below
+  
+  if (crewStatus && computerStatusCode === 200 && spaceSuitsOn) {
+    console.log("all systems go");
+ } else {
+    console.log("WARNING. Not ready");
+ }
+
+ if (!crewStatus || computerStatusCode !== 200 || !spaceSuitsOn) {
+    console.log("WARNING. Not ready");
+ } else {
+    console.log("all systems go");
+ }
+
+
+  if (crewStatus && computerStatusCode === 200 && spaceSuitsOn) {
+    console.log("all systems go");
+ } else {
+    console.log("WARNING. Not ready");
+ }
+
+ if (fuelLevel < 1000 || engineTemperature > 3500 || engineIndicatorLight === "red blinking"){
+    console.log("ENGINE FAILURE IMMINENT!");
+  } else if (fuelLevel <= 5000 || engineTemperature > 2500){
+    console.log("Check fuel level. Engines running hot.");
+  } else if (fuelLevel > 20000 && engineTemperature <= 2500){
+    console.log("Full tank. Engines good.");
+  } else if (fuelLevel > 10000 && engineTemperature <= 2500){
+    console.log("Fuel level above 50%. Engines good.");
+  } else if (fuelLevel > 5000 && engineTemperature <= 2500){
+    console.log("Fuel level above 25%. Engines good.");
+  } else {
+    console.log("Fuel and engine status pending...");
+  }
+
+  
