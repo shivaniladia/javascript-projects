e51a4df202fe01541dfd38f01b19d560203d5076
Shivani Ladia <154580687+shivaniladia@users.noreply.github.com>
2024-01-22 16:10:45 -0600

    modified:   loops/chapter-examples/for-Loop-Practice-With-Arrays.js

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,14 +1,4 @@
-// create an array variable containing the names
-let names = ["Shiavni", "Nitn", "Aarav","GrandPa","GrandMa"];
-for (let i = 0; i < names.length; i++){
-    console.log(names[i]);
-}
-// write a for loop that prints each name on a different line
-for (let i = 3; i < 10; i++) {
-    console.log(i);
- }
- let name = "LaunchCode";
 
-for (let i = 6; i < name.length; i++) {
-   console.log(name[i]);
-}
+for (let i = 0; i < -1; i++) {
+    console.log("LaunchCode");
+ }
