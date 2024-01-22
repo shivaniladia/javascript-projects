2d7cb3c9d1b228f83dee03f86708b06963f3b633
Shivani Ladia <154580687+shivaniladia@users.noreply.github.com>
2024-01-22 16:15:51 -0600

    modified:   loops/chapter-examples/Loop-Variable.js

--- loops/chapter-examples/Loop-Variable.js
+++ loops/chapter-examples/Loop-Variable.js
@@ -1,5 +1,5 @@
 // Experiment with this loop by modifying each of the following: the variable initialization, the boolean condition, and the update expression.
 
-for (let i = 0; i < 51; i++) {
-   console.log(i);
+for (let i = 10; i < 25; i + 1) {
+   console.log("number print",i);
  }
