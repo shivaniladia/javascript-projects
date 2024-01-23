214326a58983b8800f14e482ce577aa71bf0dec6
Shivani Ladia <154580687+shivaniladia@users.noreply.github.com>
2024-01-22 18:09:19 -0600

    modified:   loops/chapter-examples/for-Loop-Practice-With-Arrays.js 	modified:   loops/exercises/for-Loop-Exercises.js

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,4 +1,9 @@
+let n = 10;
+let answer = 1;
 
-for (let i = 0; i < -1; i++) {
-    console.log("LaunchCode");
- }
+while (n > 0) {
+   answer = answer + n;
+   n = n + 1;
+}
+
+console.log(answer);
