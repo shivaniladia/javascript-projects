25d122fbd2445dfb4c8aea4fdd492d24692d8c3c
Shivani Ladia <154580687+shivaniladia@users.noreply.github.com>
2024-02-15 15:46:53 -0600

    modified:   errors-and-debugging/exercises/Debugging1stSyntaxError.js

--- errors-and-debugging/exercises/Debugging1stSyntaxError.js
+++ errors-and-debugging/exercises/Debugging1stSyntaxError.js
@@ -1,10 +1,10 @@
 //Run this code first and examine the error message.
 //Fix the syntax error then run the code again to check your work.
 
-let launchReady = false;
+let launchReady = "false";
 let fuelLevel = 17000;
 
-if (fuelLevel >= 20000 {
+if (fuelLevel >= 20000){
    console.log('Fuel level cleared.');
    launchReady = true;
 } else {
