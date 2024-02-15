372fa3ac2575f4b4e8e8d5402d0c0c46e5301149
Shivani Ladia <154580687+shivaniladia@users.noreply.github.com>
2024-02-15 17:24:30 -0600

    modified:   errors-and-debugging/exercises/DebuggingLogicErrors4.js

--- errors-and-debugging/exercises/DebuggingLogicErrors4.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors4.js
@@ -29,9 +29,9 @@ if (crewStatus && computerStatus === 'green'){
 
 console.log("launchReady = ", launchReady);
 
-// if (launchReady) {
-//    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
-//    console.log('Liftoff!');
-// } else {
-//    console.log('Launch scrubbed.');
-// }
+ if (launchReady) {
+    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+    console.log('Liftoff!');
+ } else {
+    console.log('Launch scrubbed.');
+}
