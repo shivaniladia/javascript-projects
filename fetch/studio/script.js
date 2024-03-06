003c5b19c6ee7c00d8d9de8119fec2b9163a3762
Shivani Ladia <154580687+shivaniladia@users.noreply.github.com>
2024-03-06 14:33:27 -0600

    modified:   html/exercises/index.html

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,13 @@
 //TODO: Add Your Code Below
+window.onload = function() {
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json')
+    .then(response => response.json())
+    .then(data => {
+        // Code to handle the data
+        console.log(data);
+    })
+    .catch(error => {
+        // Handle any errors
+        console.error('Error fetching astronauts:', error);
+    });
+};
